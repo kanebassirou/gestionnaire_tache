4d29849987a897df850b2893fb4f39d40ea89b9c
kanebassirou <kaneb1927@gmail.com>
2023-11-13 18:01:51 +0000

    utilisation du hook useEffet

--- src/App.js
+++ src/App.js
@@ -1,93 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductCatagorieRow } from "./Produits/ProductCatagorieRow";
 import { ProductRow } from "./Produits/ProductRow";
 import { Checkbox } from "./components/forms/Checkbox";
 import { Input } from "./components/forms/Input";
 
-const PRODUCTS = [
-  {category : "fruit",   price:"$1",stoked :true,name:"Apple"},
-  {category : "electronique",price:"$100",stoked :false,name:"SAMSUNG"},
-  {category : "Vegetables",price:"$2",stoked :true,name:"PITEL"},
-  {category : "fruit",price:"$1",stoked :true,name:"Apple"}
-]
+
 function App() {
-  const [showStockedOnly, setShowStockedOnly] = useState(false);
-  const [search, setSearch] = useState('');
+  const [duration ,setDuration] = useState(5)
+  const [secondsLeft ,SetsecondsLeft] = useState(duration);
+  const handleChange = (v) => {
+    setDuration(v)
+    SetsecondsLeft(v)
+  }  
 
-  const visibleProducts = PRODUCTS.filter(product => {
-    console.log("Product:", product);
-    console.log("Search:", search);
-    if (showStockedOnly && !product.stoked) {
-      return false;
+  useEffect(()=>{
+    SetsecondsLeft(duration)
+    const timer =  setInterval(()=>{
+      SetsecondsLeft(v => {
+        if (v <= 1){ 
+          clearInterval(timer)
+          return 0
+        }
+        return v-1})
+    },1000)
+    return() => {
+      clearInterval(timer)
     }
-    if (search && !product.name.includes(search)) {
-      return false;
-    }
-    return true;
-  });
+  },[duration]);
+  return <div className="vstack gap-2">
+    <Input
+    value={duration}
+    onChange={handleChange}
+    placeholder="Timer ..."
+    />
+    <p>
+        Decompte : {secondsLeft}   
+    </p>
+    
+  </div>
 
-  console.log("Rendering App - search:", search);
-
-  return (
-    <div className="container my-3">
-      <SearchBar
-        search={search}
-        onSearchChange={setSearch}
-        showStockedOnly={showStockedOnly}
-        onStockedOnlyChange={setShowStockedOnly}
-      />
-      <ProductTable products={visibleProducts} />
-    </div>
-  );
 }
 
-  // ...
-
-  function SearchBar({ showStockedOnly, onStockedOnlyChange, search, onSearchChange }) {
-    console.log("SearchBar - search:", search);
-  
-    return (
-      <div>
-        <div className="mb-3">
-          <Input
-            value={search}
-            onChange={onSearchChange}
-            placeholder="Rechercher ..."
-          />
-          <Checkbox
-            id="stocked"
-            checked={showStockedOnly}
-            onChange={onStockedOnlyChange}
-            label="N'afficher que les produits en stock"
-          />
-        </div>
-      </div>
-    );
-  }
-
-
-  function ProductTable ({products}){
-    const rows = []
-    let lastCatagory = null
-    for(let product of products){
-      if(product.category !== lastCatagory){
-        rows.push(<ProductCatagorieRow key={product.category} name={product.category}/>)
-      }
-      lastCatagory = product.category
-      rows.push(<ProductRow product={product} key={product.name}/>)
-
-    }
-    return <table className="table">
-      <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Prix</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  }
-    
 export default App;
