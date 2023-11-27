25d5099f6e6d8104a998d79f9d0885a1dc7e8ede
kanebassirou <kaneb1927@gmail.com>
2023-11-27 21:00:18 +0000

    les route avec react

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import { useEffect, useState } from "react";
-import { ProductCatagorieRow } from "./Produits/ProductCatagorieRow";
-import { ProductRow } from "./Produits/ProductRow";
-import { Checkbox } from "./components/forms/Checkbox";
-import { Input } from "./components/forms/Input";
+import React from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
+function Home() {
+  return <h2>Accueil</h2>;
+}
 
+function About() {
+  return <h2>À propos</h2>;
+}
+function Contact() {
+  return <h2>Nos contact</h2>;
+
+}
+function User() {
+  let { userId }  = useParams();
+  return <h2>Utilisateur {userId}</h2>;
+}
 function App() {
-  const [duration ,setDuration] = useState(5)
-  const [secondsLeft ,SetsecondsLeft] = useState(duration);
-  const handleChange = (v) => {
-    setDuration(v)
-    SetsecondsLeft(v)
-  }  
-
-  useEffect(()=>{
-    SetsecondsLeft(duration)
-    const timer =  setInterval(()=>{
-      SetsecondsLeft(v => {
-        if (v <= 1){ 
-          clearInterval(timer)
-          return 0
-        }
-        return v-1})
-    },1000)
-    return() => {
-      clearInterval(timer)
-    }
-  },[duration]);
-  return <div className="vstack gap-2">
-    <Input
-    value={duration}
-    onChange={handleChange}
-    placeholder="Timer ..."
-    />
-    <p>
-        Decompte : {secondsLeft}   
-    </p>
-    
-  </div>
-
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Accueil</Link></li>
+            <li><Link to="/about">À propos</Link></li>
+            <li><Link to="/contact">Nos contact</Link></li>
+           <li><Link to="/user/1">Utilisateur 1</Link></li>
+           <li><Link to="/user/2">Utilisateur 2</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/user/:userId" element ={<User />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
